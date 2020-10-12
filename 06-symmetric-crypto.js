42ff84d16e93180c9f9898fea8f080b4915da653
getlarge <ed@getlarge.eu>
2020-10-12 08:53:16 +0200

    add symmetric crypto examples using streams

--- 06-symmetric-crypto.js
+++ 06-symmetric-crypto.js
@@ -1,7 +1,22 @@
 const assert = require('assert');
 const crypto = require('crypto');
+const combine = require('multipipe');
+const {
+  measureExecutionTime,
+  getInputBuffer,
+  getReadableStream,
+  appendToStream,
+  removeFromStream,
+} = require('./utils');
 
 // 6 - SYMMETRIC ENCRYPTION - DECRYPTION
+function getScryptKey({ password, salt, keyLength }) {
+  return new Promise((resolve, reject) => {
+    crypto.scrypt(password, salt, keyLength, (err, key) => {
+      err ? reject(err) : resolve(key);
+    });
+  });
+}
 
 // AES-256-CTR
 async function aes_256_ctr(plainText = 'secretMsg', password = 'p@sSw0rd~123') {
@@ -58,81 +73,319 @@ async function aes_256_ctr(plainText = 'secretMsg', password = 'p@sSw0rd~123') {
   return { encryptedText, decryptedText };
 }
 
+function aes_256_ctr_stream(
+  input = 'secretMsg',
+  password = 'p@sSw0rd~123',
+  options = {
+    encoding: {
+      input: 'utf8',
+    },
+  }
+) {
+  const algorithm = 'aes-256-ctr';
+  const passwordSalt = crypto.randomBytes(16);
+  const key = crypto.scryptSync(password, passwordSalt, 32);
+  const iv = crypto.randomBytes(16);
+
+  input = getReadableStream(input);
+
+  return combine(
+    input
+      .pipe(crypto.createCipheriv(algorithm, key, iv))
+      .pipe(crypto.createDecipheriv(algorithm, key, iv))
+  );
+}
+
 // KDF + CHACHA20-POLY1305
 async function chacha20_poly1305(
-  plainText = 'secretMsg',
-  password = 'p@sSw0rd~123'
+  input = 'secretMsg',
+  password = 'p@sSw0rd~123',
+  options = {
+    algorithm: 'chacha20-poly1305',
+    encoding: {
+      input: 'utf8',
+    },
+    ivLength: 12,
+    authTagLength: 16,
+    keyLength: 32,
+    saltLength: 16,
+    assocData: Buffer.alloc(16, 0xaa),
+  }
 ) {
-  const algorithm = 'chacha20-poly1305';
-  let assocData = Buffer.alloc(16, 0xaa);
+  async function getEncryptionParam(
+    password,
+    { assocData, ivLength, saltLength, keyLength }
+  ) {
+    const iv = crypto.randomBytes(ivLength);
+    const salt = crypto.randomBytes(saltLength);
+    const key = await getScryptKey({ password, salt, keyLength });
+    return { key, iv, salt, assocData };
+  }
 
-  function encrypt({ password, plainText }) {
-    const iv = crypto.randomBytes(12);
-    const passwordSalt = crypto.randomBytes(16);
-    const key = crypto.scryptSync(password, passwordSalt, 32);
-    let encryptedText = '';
-
+  function encrypt(
+    input,
+    { key, salt, iv },
+    { algorithm, assocData, encoding, authTagLength }
+  ) {
     return new Promise((resolve) => {
       const cipher = crypto
         .createCipheriv(algorithm, key, iv, {
-          authTagLength: 16,
+          authTagLength,
         })
         .setAAD(assocData);
 
-      cipher.on('readable', () => {
-        let chunk;
-        while (null !== (chunk = cipher.read())) {
-          encryptedText += chunk.toString('hex');
-        }
-      });
-
-      cipher.on('end', () => {
-        const authTag = cipher.getAuthTag();
-        resolve({ encryptedText, iv, authTag, salt: passwordSalt });
-      });
-
-      cipher.write(plainText);
-      cipher.end();
+      let encryptedBuffer = cipher.update(input);
+      cipher.final();
+      const authTag = cipher.getAuthTag();
+      encryptedBuffer = Buffer.concat(
+        [iv, salt, authTag, encryptedBuffer],
+        iv.length + salt.length + authTag.length + encryptedBuffer.length
+      );
+      resolve(encryptedBuffer);
     });
   }
 
-  function decrypt({ password, salt, encryptedText, iv, authTag }) {
-    const key = crypto.scryptSync(password, salt, 32);
-    let decryptedText = '';
+  async function getDecryptionParam(
+    input,
+    password,
+    { assocData, ivLength, saltLength, keyLength, authTagLength }
+  ) {
+    const iv = input.slice(0, ivLength);
+    const salt = input.slice(ivLength, ivLength + saltLength);
+    const authTag = input.slice(
+      ivLength + saltLength,
+      ivLength + saltLength + authTagLength
+    );
+    const key = await getScryptKey({ password, salt, keyLength });
+    return { key, iv, salt, assocData, authTag };
+  }
+
+  function decrypt(
+    input,
+    { key, iv, authTag },
+    { algorithm, assocData, authTagLength, ivLength, saltLength }
+  ) {
+    let decryptedData = '';
+    const cipherBuffer = input.slice(ivLength + saltLength + authTagLength);
 
     return new Promise((resolve, reject) => {
       const decipher = crypto
         .createDecipheriv(algorithm, key, iv, {
-          authTagLength: 16,
+          authTagLength,
         })
         .setAAD(assocData);
 
-      decryptedText = decipher.update(encryptedText, 'hex', 'utf8');
+      decryptedData = decipher.update(cipherBuffer);
       try {
         decipher.setAuthTag(authTag);
-        decryptedText += decipher.final('utf8');
-        resolve(decryptedText);
+        const lastChunk = decipher.final();
+        decryptedData = Buffer.concat(
+          [decryptedData, lastChunk],
+          decryptedData.length + lastChunk.length
+        );
+        resolve(decryptedData);
       } catch (e) {
         reject(e);
       }
     });
   }
 
-  const { encryptedText, iv, authTag, salt } = await encrypt({
-    password,
-    plainText,
-  });
-  const decryptedText = await decrypt({
-    password,
-    salt,
-    encryptedText,
-    iv,
-    authTag,
-  });
+  const hrstart = process.hrtime();
+  input = getInputBuffer(input, options.encoding);
 
-  assert.strictEqual(plainText, decryptedText);
+  const encryptionParam = await getEncryptionParam(password, options);
+  measureExecutionTime(hrstart, 'getEncryptionParam');
+  const encryptedData = await encrypt(input, encryptionParam, options);
+  measureExecutionTime(hrstart, 'read encryptData');
 
-  return { encryptedText, decryptedText };
+  const decryptionParam = await getDecryptionParam(
+    encryptedData,
+    password,
+    options
+  );
+  measureExecutionTime(hrstart, 'getDecryptionParam');
+  const decryptedData = await decrypt(encryptedData, decryptionParam, options);
+  measureExecutionTime(hrstart, 'read decryptData');
+
+  assert.strictEqual(input.toString('utf8'), decryptedData.toString('utf8'));
+
+  return {
+    encryptedText: encryptedData.toString('hex'),
+    decryptedText: decryptedData.toString('utf8'),
+  };
+}
+
+async function chacha20_poly1305_stream(
+  input = 'secretMsg',
+  password = 'p@sSw0rd~123',
+  options = {
+    algorithm: 'chacha20-poly1305',
+    encoding: {
+      input: 'utf8',
+    },
+    ivLength: 12,
+    authTagLength: 16,
+    keyLength: 32,
+    saltLength: 16,
+    assocData: Buffer.alloc(16, 0xaa),
+  }
+) {
+  async function getEncryptionParam(
+    password,
+    { assocData, ivLength, saltLength, keyLength }
+  ) {
+    const iv = crypto.randomBytes(ivLength);
+    const salt = crypto.randomBytes(saltLength);
+    const key = await getScryptKey({ password, salt, keyLength });
+    return { key, iv, salt, assocData };
+  }
+
+  function encryptStream({ key, iv }, { algorithm, assocData, authTagLength }) {
+    return crypto
+      .createCipheriv(algorithm, key, iv, {
+        authTagLength,
+      })
+      .setAAD(assocData);
+  }
+
+  function encryptChain(
+    readable,
+    { key, salt, iv },
+    { algorithm, assocData, encoding, authTagLength }
+  ) {
+    const appendIvAndSalt = appendToStream(
+      Buffer.concat([iv, salt], iv.length + salt.length)
+    );
+    const cipher = encryptStream(
+      { iv, key },
+      { algorithm, assocData, authTagLength }
+    );
+    return combine(readable.pipe(cipher).pipe(appendIvAndSalt));
+  }
+
+  // simulate loading from storage of ciphered data as a stream
+  async function getDecryptionParam(
+    readable,
+    password,
+    { assocData, ivLength, saltLength, keyLength }
+  ) {
+    const { iv, salt } = await new Promise((resolve, reject) => {
+      let buffer = Buffer.alloc(0);
+      const struct = {
+        iv: Buffer.alloc(0),
+        salt: Buffer.alloc(0),
+        // authTag: Buffer.alloc(0),
+      };
+
+      readable.on('error', (error) => {
+        reject(error);
+      });
+
+      readable.on('data', (data) => {
+        while (buffer.length <= ivLength + saltLength) {
+          buffer = Buffer.concat([buffer, data], buffer.length + data.length);
+        }
+        // readable.destroy();
+        readable.pause();
+        struct.iv = buffer.slice(0, ivLength);
+        struct.salt = buffer.slice(ivLength, ivLength + saltLength);
+        resolve(struct);
+      });
+    });
+
+    const key = await getScryptKey({ password, salt, keyLength });
+    return { key, iv, salt, assocData };
+  }
+
+  function decryptStream(
+    { key, iv, authTag },
+    { algorithm, assocData, authTagLength }
+  ) {
+    return crypto
+      .createDecipheriv(algorithm, key, iv, {
+        authTagLength,
+      })
+      .setAAD(assocData);
+    // // setAuthTag(authTag);
+  }
+
+  function decryptChain(
+    readable,
+    { key, iv },
+    { algorithm, assocData, authTagLength, ivLength, saltLength }
+  ) {
+    const removeIvAndSalt = removeFromStream(ivLength + saltLength);
+    const decipher = decryptStream(
+      { iv, key },
+      { algorithm, assocData, authTagLength }
+    );
+    return combine(readable.pipe(removeIvAndSalt).pipe(decipher));
+  }
+
+  const hrstart = process.hrtime();
+
+  const inputReadable = getReadableStream(input, options.encoding, 16);
+  const encryptionParam = await getEncryptionParam(password, options);
+  measureExecutionTime(hrstart, 'getEncryptionParam');
+  const encryptChainStream = encryptChain(
+    inputReadable,
+    encryptionParam,
+    options
+  );
+  measureExecutionTime(hrstart, 'encryptChain');
+
+  // simulate storage of ciphered data
+  const encryptedData = await new Promise((resolve, reject) => {
+    let encryptedData = Buffer.alloc(0);
+    encryptChainStream
+      .on('error', (error) => {
+        reject(error);
+      })
+      .on('end', () => {
+        resolve(encryptedData);
+      })
+      .on('data', (data) => {
+        encryptedData = Buffer.concat(
+          [encryptedData, data],
+          encryptedData.length + data.length
+        );
+      });
+  });
+  measureExecutionTime(hrstart, 'read encryptedData');
+
+  let readable = getReadableStream(encryptedData);
+
+  const decryptionParam = await getDecryptionParam(readable, password, options);
+  measureExecutionTime(hrstart, 'getDecryptionParam');
+
+  readable = getReadableStream(encryptedData);
+  const decryptChainStream = decryptChain(readable.clone(), decryptionParam, options);
+  measureExecutionTime(hrstart, 'decryptChain');
+
+  const decryptedData = await new Promise((resolve, reject) => {
+    let decryptedData = Buffer.alloc(0);
+    decryptChainStream
+      .on('error', (error) => {
+        reject(error);
+      })
+      .on('end', () => {
+        resolve(decryptedData);
+      })
+      .on('data', (data) => {
+        decryptedData = Buffer.concat(
+          [decryptedData, data],
+          decryptedData.length + data.length
+        );
+      });
+  });
+  measureExecutionTime(hrstart, 'read decryptedData');
+
+  assert.strictEqual(input.toString('utf8'), decryptedData.toString('utf8'));
+
+  return {
+    encryptedText: encryptedData.toString('hex'),
+    decryptedText: decryptedData.toString('utf8'),
+  };
 }
 
 // KDF + AES-256-CBC + HMAC
@@ -259,6 +512,8 @@ async function aes_256_cbc_hmac(
 
 module.exports = {
   aes_256_ctr,
+  aes_256_ctr_stream,
   chacha20_poly1305,
+  chacha20_poly1305_stream,
   aes_256_cbc_hmac,
 };
---
+++ utils.js
@@ -0,0 +1,76 @@
+const cloneable = require('cloneable-readable');
+const { Readable, Transform } = require('stream');
+
+function measureExecutionTime(hrstart, fn = '') {
+  const hrend = process.hrtime(hrstart);
+  console.info(
+    `Execution time for ${fn} : %ds %dms`,
+    hrend[0],
+    hrend[1] / 1000000
+  );
+}
+
+function getInputBuffer(input, encoding) {
+  if (typeof input === 'string' && typeof encoding.input === 'string') {
+    return Buffer.from(input, encoding.input);
+  }
+  return input;
+}
+
+function getReadableStream(input, encoding = null, chunkSize = 32) {
+  async function* generate(input, chunkSize) {
+    const len = input.length;
+    let i = 0;
+    while (i < len) {
+      yield input.slice(i, (i += chunkSize));
+    }
+  }
+
+  if (!(input instanceof Readable)) {
+    const readable = Readable.from(generate(input, chunkSize));
+    return cloneable(readable);
+  }
+  return cloneable(input);
+}
+
+const appendToStream = (value) => {
+  let appended = false;
+
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      if (!appended) {
+        this.push(value);
+        appended = true;
+      }
+      this.push(chunk);
+      callback();
+    },
+  });
+};
+
+const removeFromStream = (length) => {
+  let buffer = Buffer.alloc(0);
+  let removed = false;
+
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      if (!removed || buffer.length <= length) {
+        buffer = Buffer.concat([buffer, chunk], buffer.length + chunk.length);
+        const partChunk = buffer.slice(length);
+        this.push(partChunk);
+        removed = true;
+      } else {
+        this.push(chunk);
+      }
+      callback();
+    },
+  });
+};
+
+module.exports = {
+  measureExecutionTime,
+  getInputBuffer,
+  getReadableStream,
+  appendToStream,
+  removeFromStream,
+};
